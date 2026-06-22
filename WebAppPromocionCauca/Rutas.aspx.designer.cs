e45419f820c4e41f0a06c6696d91b2d03c6005fa
rodney341 <huilyrg@gmail.com>
2026-06-21 23:51:55 -0500

    ajuste rutas

--- WebAppPromocionCauca/Rutas.aspx.designer.cs
+++ WebAppPromocionCauca/Rutas.aspx.designer.cs
@@ -13,14 +13,5 @@ namespace WebAppPromocionCauca
 
     public partial class Rutas
     {
-
-        /// <summary>
-        /// lnkRegresarInicio control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink lnkRegresarInicio;
     }
 }
